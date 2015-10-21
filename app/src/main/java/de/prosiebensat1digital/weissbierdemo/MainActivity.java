27d762d6d26f09ce91fd0a127c343b0e9f8eb730
Thomas Paul Mann <hello@thomaspaulmann.com>
2015-10-21 09:32:40 +0200

    Initial commit

---
+++ app/src/main/java/de/prosiebensat1digital/weissbierdemo/MainActivity.java
@@ -0,0 +1,13 @@
+package de.prosiebensat1digital.weissbierdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
