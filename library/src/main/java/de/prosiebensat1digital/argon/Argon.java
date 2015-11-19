0f860aba5fcd4ab0f163e650005a87b0883d2e87
Wolfgang Karl <kontakt@wkarl.net>
2015-11-19 12:42:34 +0100

    Move hard-coded strings to strings.xml

--- library/src/main/java/de/prosiebensat1digital/argon/Argon.java
+++ library/src/main/java/de/prosiebensat1digital/argon/Argon.java
@@ -5,6 +5,8 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.v4.app.NotificationCompat;
 
@@ -17,7 +19,7 @@ public class Argon {
     private static final int REQUEST_CODE    = 0;
     
     private Context mContext;
-    
+
     private Argon(Context inContext) {
         mContext = inContext;
     }
@@ -49,8 +51,8 @@ public class Argon {
         return new NotificationCompat.Builder(mContext)
                 .setOngoing(true)
                 .setSmallIcon(R.drawable.ic_debug)
-                .setContentTitle("Debug Mode")
-                .setContentText("Let's do some debug configuring!")
+                .setContentTitle(mContext.getString(R.string.notification_title))
+                .setContentText(mContext.getString(R.string.notification_text))
                 .setContentIntent(buildContentIntent())
                 .build();
     }
@@ -59,5 +61,25 @@ public class Argon {
         Intent intent = new Intent(mContext, ArgonActivity.class);
 
         return PendingIntent.getActivity(mContext, REQUEST_CODE, intent, 0);
+    }
+
+    public String getString(String key, String defaultValue) {
+        return PreferenceManager.getDefaultSharedPreferences(mContext).getString(key, defaultValue);
+    }
+
+    public boolean getBoolean(String key, boolean defaultValue) {
+        return PreferenceManager.getDefaultSharedPreferences(mContext).getBoolean(key, defaultValue);
+    }
+
+    public int getInt(String key, int defaultValue) {
+        return PreferenceManager.getDefaultSharedPreferences(mContext).getInt(key, defaultValue);
+    }
+
+    public float getFloat(String key, float defaultValue) {
+        return PreferenceManager.getDefaultSharedPreferences(mContext).getFloat(key, defaultValue);
+    }
+
+    public long getLong(String key, long defaultValue) {
+        return PreferenceManager.getDefaultSharedPreferences(mContext).getLong(key, defaultValue);
     }
 }
