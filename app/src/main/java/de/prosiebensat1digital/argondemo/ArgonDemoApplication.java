289e6c1f25112c448f6b5e12141fa2d93ad94b9f
Daniel Cachapa <cachapa@gmail.com>
2015-11-23 15:44:30 +0100

    Make notification configurable

--- app/src/main/java/de/prosiebensat1digital/argondemo/ArgonDemoApplication.java
+++ app/src/main/java/de/prosiebensat1digital/argondemo/ArgonDemoApplication.java
@@ -8,6 +8,7 @@ public class ArgonDemoApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        Argon.init(this, R.xml.preferences);
+    
+        Argon.init(this, R.xml.preferences, R.drawable.ic_debug, R.string.app_name);
     }
 }
--- library/src/main/java/de/prosiebensat1digital/argon/Argon.java
+++ library/src/main/java/de/prosiebensat1digital/argon/Argon.java
@@ -7,100 +7,110 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.DrawableRes;
 import android.support.annotation.NonNull;
+import android.support.annotation.StringRes;
+import android.support.annotation.XmlRes;
 import android.support.v4.app.NotificationCompat;
 
 import java.util.Map;
 
-/**
- * Created by Thomas Mann on 21/10/15.
- */
 public class Argon {
     static final String ARGON_PREFERENCES_RESOURCE = "de.prosiebensat1digital.argon.PREFERENCES_RESOURCE";
-    static final String ARGON_PREFERENCES = "de.prosiebensat1digital.argon.PREFERENCES";
+    static final String ARGON_PREFERENCES          = "de.prosiebensat1digital.argon.PREFERENCES";
+    
     private static final int NOTIFICATION_ID = 666;
     private static final int REQUEST_CODE    = 0;
-
+    
     private static Argon sInstance;
-
+    
     private Context mContext;
-    private int mPreferenceResourceId;
-
-    private Argon(Context inContext, int preferenceResourceId) {
+    
+    @XmlRes
+    private       int mPreferenceResourceId;
+    @DrawableRes
+    private final int mNotificationIconRes;
+    @StringRes
+    private final int mTitleRes;
+    
+    private Argon(@NonNull Context inContext, @XmlRes int preferenceResourceId,
+                  @DrawableRes int notificationIconRes, @StringRes int titleRes) {
         mContext = inContext;
         mPreferenceResourceId = preferenceResourceId;
+        mTitleRes = titleRes;
+        mNotificationIconRes = notificationIconRes;
     }
-
+    
     /* setup */
-
-    public static void init(@NonNull Application application, int preferenceResourceId) {
-        sInstance = new Argon(application, preferenceResourceId);
+    public static void init(@NonNull Application application, @XmlRes int preferenceResourceId,
+                            @DrawableRes int notificationIconRes, @StringRes int titleRes) {
+        sInstance = new Argon(application, preferenceResourceId, notificationIconRes, titleRes);
     }
-
+    
     public static Argon getInstance() {
         if (sInstance == null) {
             throw new IllegalStateException("Please set up Argon in your Application class using Argon.init(Application, int).");
         }
         return sInstance;
     }
-
+    
     public Argon start() {
         Notification notification = buildNotification();
         getNotificationManager().notify(NOTIFICATION_ID, notification);
         return this;
     }
-
+    
     public void stop() {
         getNotificationManager().cancel(NOTIFICATION_ID);
     }
 
     /* helpers */
-
+    
     private NotificationManager getNotificationManager() {
         return (NotificationManager) mContext.getSystemService(Context.NOTIFICATION_SERVICE);
     }
-
+    
     private Notification buildNotification() {
         return new NotificationCompat.Builder(mContext)
                 .setOngoing(true)
-                .setSmallIcon(R.drawable.ic_debug)
-                .setContentTitle(mContext.getString(R.string.notification_title))
+                .setSmallIcon(mNotificationIconRes)
+                .setContentTitle(mContext.getString(mTitleRes))
                 .setContentText(mContext.getString(R.string.notification_text))
                 .setContentIntent(buildContentIntent())
                 .build();
     }
-
-    private PendingIntent buildContentIntent(){
+    
+    private PendingIntent buildContentIntent() {
         Intent intent = new Intent(mContext, ArgonActivity.class);
         intent.putExtra(ARGON_PREFERENCES_RESOURCE, mPreferenceResourceId);
         intent.setAction("ArgonActivity");
         return PendingIntent.getActivity(mContext, REQUEST_CODE, intent, 0);
     }
-
+    
     public String getString(String key, String defaultValue) {
         return mContext.getSharedPreferences(ARGON_PREFERENCES, Context.MODE_PRIVATE).getString(key, defaultValue);
     }
-
+    
     public boolean getBoolean(String key, boolean defaultValue) {
         return mContext.getSharedPreferences(ARGON_PREFERENCES, Context.MODE_PRIVATE).getBoolean(key, defaultValue);
     }
-
+    
     public int getInt(String key, int defaultValue) {
         return mContext.getSharedPreferences(ARGON_PREFERENCES, Context.MODE_PRIVATE).getInt(key, defaultValue);
     }
-
+    
     public float getFloat(String key, float defaultValue) {
         return mContext.getSharedPreferences(ARGON_PREFERENCES, Context.MODE_PRIVATE).getFloat(key, defaultValue);
     }
-
+    
     public long getLong(String key, long defaultValue) {
         return mContext.getSharedPreferences(ARGON_PREFERENCES, Context.MODE_PRIVATE).getLong(key, defaultValue);
     }
-
+    
     public void setFeatureFlags(Map<String, Boolean> featureFlags) {
-        Intent intent = new Intent(mContext, ArgonActivity.class);
+        Intent intent      = new Intent(mContext, ArgonActivity.class);
         Bundle flagsBundle = new Bundle();
-
+        
         for (Map.Entry<String, Boolean> entry : featureFlags.entrySet()) {
             flagsBundle.putBoolean(entry.getKey(), entry.getValue());
         }
