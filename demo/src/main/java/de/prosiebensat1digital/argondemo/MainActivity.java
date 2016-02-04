141f4625f3c134faeb222e8366e1add540ac54e5
Wolfgang Karl <kontakt@wkarl.net>
2016-02-04 11:09:56 +0100

    Add another activity to demonstrate notification lifecycle handling

--- demo/src/main/java/de/prosiebensat1digital/argondemo/MainActivity.java
+++ demo/src/main/java/de/prosiebensat1digital/argondemo/MainActivity.java
@@ -1,15 +1,15 @@
 package de.prosiebensat1digital.argondemo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
 import de.prosiebensat1digital.argon.Argon;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
     private ConfigModel mConfig;
     
     private TextView mHeadlineView;
@@ -25,26 +25,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         // Find views
         mHeadlineView = (TextView) findViewById(R.id.headline);
         
-        TextView stringView   = ((TextView) findViewById(R.id.stringText));
-        TextView intView      = ((TextView) findViewById(R.id.integerText));
-        TextView floatView    = ((TextView) findViewById(R.id.floatText));
-        TextView longView     = ((TextView) findViewById(R.id.longText));
-        Button   toggleButton = (Button) findViewById(R.id.toggleButton);
-        
         mHeadlineView.setText(mConfig.showHeadline ? "Hello World!" : null);
-        
-        stringView.setText(mConfig.text);
-        intView.setText(String.valueOf(mConfig.intValue));
-        floatView.setText(String.valueOf(mConfig.floatValue));
-        longView.setText(String.valueOf(mConfig.longValue));
-        toggleButton.setOnClickListener(this);
     }
-    
-    @Override
-    public void onClick(View v) {
+
+    public void toggleHeadline(View v) {
         mConfig.showHeadline = TextUtils.isEmpty(mHeadlineView.getText());
-        
+
         Argon.updateConfig(mConfig);
         Argon.restartProcess();
+    }
+
+    public void goToDetails(View v) {
+        Intent intent = new Intent(this, DetailsActivity.class);
+        startActivity(intent);
     }
 }
